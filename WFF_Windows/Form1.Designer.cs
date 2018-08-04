30280028ddce1133ed00a82af0ac134bce731d11
Ghada Shafiq <shafiq.ghada@gmail.com>
2018-08-04 21:36:57 +0200

    first commit

---
+++ WFF_Windows/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WFF_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.check = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(58, 93);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(11, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the expression : \r\n\"| for or , alt+26 for → , alt+29 for ↔\"";
+            // 
+            // check
+            // 
+            this.check.BackColor = System.Drawing.Color.Red;
+            this.check.Font = new System.Drawing.Font("Rockwell Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.check.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.check.Location = new System.Drawing.Point(106, 133);
+            this.check.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(64, 37);
+            this.check.TabIndex = 2;
+            this.check.Text = "Check!";
+            this.check.UseVisualStyleBackColor = false;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Rockwell Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.Green;
+            this.result.Location = new System.Drawing.Point(103, 195);
+            this.result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 15);
+            this.result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(293, 261);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Rockwell Condensed", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Check PL Syntax";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ WFF_Windows/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFF_Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.BorderStyle = BorderStyle.Fixed3D;
+            
+           
+        }
+
+        static bool ParenMatch(string x)
+        {
+            Stack<char> S = new Stack<char>(100);
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == '(')
+                {
+                    S.Push(x[i]);
+                }
+                else if (x[i] == ')')
+                {
+                    if (S.IsEmpty())
+                    {
+                        return false;
+                    }
+                    //(: 40 ): 41
+                    int j = ((int)S.Peek()) + 1;
+                    if ((int)x[i] != j)
+                        return false;
+
+                    S.Pop();
+                }
+
+            }
+            if (S.IsEmpty())
+                return true;
+            else
+                return false;
+
+        }
+       
+        private void check_Click(object sender, EventArgs e)
+        {
+            string exp = textBox1.Text;
+            bool chk = Check(exp);
+            if (chk)
+                result.Text = "WFF";
+            else
+                result.Text = "NOT WFF";
+        }
+        static bool Check(string exp)
+        {
+
+
+            if (!ParenMatch(exp))
+                return false;
+
+            if (!Digits(exp))
+                return false;
+
+            if (String.IsNullOrWhiteSpace(exp))
+                return false;
+
+            if (!(exp[0] == '(' || exp[0] == '~' || exp[0] == '_' || char.IsLower(exp, 0)))
+                return false;
+
+            for (int i = 0; i < exp.Length; i++)
+            {
+
+                if (!(exp[i] == '^' || exp[i] == '|' || exp[i] == '~' ||
+                    exp[i] == '_' || exp[i] == '(' || exp[i] == ')' ||
+                    exp[i] == '→' || exp[i] == '↔' ||
+                    char.IsLower(exp, i) || char.IsDigit(exp, i)))
+                    return false;
+
+
+                if (exp[i] == ')')
+                {
+                    if (i > 0)
+                    {
+                        if (!(char.IsLower(exp,i-1)|| char.IsDigit(exp, i - 1) || exp[i-1]==')' || exp[i-1]=='_'))
+                            return false;
+                    }
+                }
+                if (exp[i] == '~')
+                {
+                    
+                    int var = exp.Length - 1;
+                    if (i == var)
+                        return false;
+                    if (i < var)
+                    {
+                        if (!(char.IsLower(exp,i+1)|| exp[i + 1] == '(' || exp[i+1]=='_'))
+                            return false;
+                    }
+                    if (i > 0)
+                    {
+                        if (char.IsLower(exp,i-1) || exp[i-1]==')')
+                            return false;
+                    }
+
+
+
+                }
+
+                //alt+26 for → , alt+29 for ↔
+
+                if (exp[i] == '^' || exp[i] == '|' || exp[i] == '→' || exp[i] == '↔')
+                {
+                    if (i == 0 || i == exp.Length - 1)
+                    {
+                        return false;
+                    }
+                    
+                    if (exp[i + 1] == '^' || exp[i + 1] == '|' || exp[i + 1] == '→' || exp[i + 1] == '↔')
+                        return false;
+                }
+                if (char.IsLower(exp,i))
+                {
+                    int var = exp.Length - 1;
+                    if (i < var)
+                    {
+                        //if it has a letter after it or ( or not , return false
+                        if (char.IsLower(exp,i+1) || exp[i + 1] == '(' || exp[i + 1] == '~')
+                            return false;
+                        //  if (!(exp[i + 1] == '_' || char.IsDigit(exp,i+1)))
+                        //  return false;
+
+                    }
+                    if(i > 0)
+                    {
+                        if (char.IsDigit(exp, i - 1))
+                            return false;
+                    }
+                }
+                if (exp[i] == '_')
+                {
+
+                    int var = exp.Length - 1;
+
+                    if (i == var)
+                    {
+                        if (i == 0)
+                            return false;
+                    }
+
+                    if (i < var)
+                    {
+
+                        if ( exp[i+1] =='(')
+                            return false;
+                    }
+                    if (i > 0)
+                    {
+                        if (exp[i - 1] == ')')
+                            return false;
+                    }
+                }
+
+
+
+            }
+            return true;
+        }
+        static bool Digits(string exp)
+        {
+            int var = exp.Length - 1;
+            for (int i = 0; i < exp.Length; i++)
+            {
+                if (char.IsDigit(exp, i))
+                {
+                    if (i > 0)
+                    {
+                        //number muxt have a letter or _ or another number before it
+                        if (!(char.IsLower(exp,i-1) || exp[i - 1] == '_' || char.IsDigit(exp, i - 1)))
+                            return false;
+                    }
+                    if(i<var)
+                    {
+                        if (char.IsLower(exp, i + 1))
+                            return false;
+                    }
+                    
+                }
+
+            }
+            return true;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    class Stack<T>
+    {
+        private T[] m_items;
+        private int top = -1;
+
+        public Stack(int capacity)
+        {
+            m_items = new T[capacity];
+        }
+
+        public int GetSize()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top < 0;
+        }
+
+        public void Push(T val)
+        {
+
+            if (GetSize() == m_items.Length)
+                throw new Exception("Sorry, Full Stack, can't add new item");
+            else
+            {
+
+                top++;
+                m_items[top] = val;
+            }
+        }
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new Exception("Empty Stack, can't remove");
+            else
+            {
+                T oldTop = m_items[top];
+                top--;
+                return oldTop;
+            }
+        }
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new Exception("Empty Stack, can't get top");
+            else
+            {
+                return m_items[top];
+            }
+        }
+    }
+}
